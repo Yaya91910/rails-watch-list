24cb2bdb6ae330b050346b315ad1f1f776aa33e0
Yassin Fekih <yassin.fekih@free.fr>
2022-08-18 16:09:46 +0200

    exo fini en back

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists do
-    resources :bookmarks, only: [:index, :new, :create]
+    resources :bookmarks, only: [:index, :new, :create ]
   end
-    resources :bookmarks, only: :destroy
+  resources :bookmarks, only: :destroy
 end
